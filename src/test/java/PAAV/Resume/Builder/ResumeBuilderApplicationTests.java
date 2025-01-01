87c476aa0216089df0e6d21fb679f4a77877e0e3
anujyadav2244 <anujyadavjnp02@gmail.com>
2025-01-01 22:27:23 +0530

    First Commit

---
+++ src/test/java/PAAV/Resume/Builder/ResumeBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package PAAV.Resume.Builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
